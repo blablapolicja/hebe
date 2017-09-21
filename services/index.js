0038f1a0f26aa3f1efdea3fcd670ae7340702b3f
blablapolicja <blablapolicja@gmail.com>
2017-09-21 18:14:08 +0300

    test request

--- services/index.js
+++ services/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const app = express();
 const HTTP_PORT = 8080;
+const token = process.env.FB_PAGE_ACCESS_TOKEN;
+
+
+const sendTextMessage = (sender, text) => {
+  const messageData = { text };
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: token,
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: sender,
+      },
+      message: messageData,
+    },
+  }, (error, response) => {
+    if (error) {
+      console.log('Error sending messages: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+};
 
 app.set('port', (process.env.PORT || HTTP_PORT));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +42,25 @@ app.get('/webhook/', (req, res) => {
   }
 
   res.send('Error, wrong token');
+});
+
+app.post('/webhook/', (req, res) => {
+  const messagingEvents = req.body.entry[0].messaging;
+
+  console.log(messagingEvents);
+  messagingEvents.map((event) => {
+    const sender = event.sender.id;
+
+    if (event.message && event.message.text) {
+      const { text } = event.message;
+
+      sendTextMessage(sender, `Text received, echo: ${text.substring(0, 200)}`);
+    }
+
+    return null;
+  });
+
+  res.sendStatus(200);
 });
 
 app.listen(app.get('port'), () => console.log('Listen http on ', app.get('port')));
